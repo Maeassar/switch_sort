a4775781191eaa02e25cf8c760c19465a729c8c2
Maeassar <2871919107@qq.com>
2023-11-30 16:57:05 +0800

    test3

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.switch_sort import switch_sort
+from src.switch import switch_sort
 import unittest
 
 class TestSwichSort(unittest.TestCase):
